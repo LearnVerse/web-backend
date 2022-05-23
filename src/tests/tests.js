245d75cdac57cc8a03d9d1dac149ed928f514d36
mabrera <mabrera@gmail.com>
2022-05-23 02:26:08 -0400

    refactored to reduce db redundances

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -8,7 +8,7 @@ let partyCode;
 (async () => {
   try {
     // Attempt to create party; EXPECT: an _id
-    const partyResponse = await axios.get('http://localhost:9090/server/createParty', { params: { name, game, numPlayers } });
+    const partyResponse = await axios.post('http://localhost:9090/server/createParty', { params: { name, game, numPlayers } });
     if (partyResponse.data.status === 200) {
       partyCode = partyResponse.data.data;
       console.log(`SUCCESS! Party created with code ${partyCode}`);
@@ -17,27 +17,27 @@ let partyCode;
     }
 
     // Attempt to add Student1 to party; EXPECT: IP address 100.100.100.100
-    const student1Response = await axios.get('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student1', partyCode } });
+    const student1Response = await axios.post('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student1', partyCode } });
     if (student1Response.data.status === 200) console.log(`SUCCESS! Student1 added to server ${student1Response.data.data}`);
     else throw new Error('ERROR! Couldn\'t add Student1 to party');
 
-    // Attempt to add Student1 to party; EXPECT: IP address 100.100.100.100
-    const student2Response = await axios.get('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student2', partyCode } });
+    // Attempt to add Student2 to party; EXPECT: IP address 100.100.100.100
+    const student2Response = await axios.post('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student2', partyCode } });
     if (student2Response.data.status === 200) console.log(`SUCCESS! Student2 added to server ${student2Response.data.data}`);
     else throw new Error('ERROR! Couldn\'t add Student1 to party');
 
-    // Attempt to add Student1 to party; EXPECT: IP address 200.200.200.200
-    const student3Response = await axios.get('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student3', partyCode } });
+    // Attempt to add Student3 to party; EXPECT: IP address 200.200.200.200
+    const student3Response = await axios.post('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student3', partyCode } });
     if (student3Response.data.status === 200) console.log(`SUCCESS! Student3 added to server ${student3Response.data.data}`);
     else throw new Error('ERROR! Couldn\'t add Student1 to party');
 
-    // Attempt to add Student1 to party; EXPECT: IP address 200.200.200.200
-    const student4Response = await axios.get('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student4', partyCode } });
+    // Attempt to add Student4 to party; EXPECT: IP address 200.200.200.200
+    const student4Response = await axios.post('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student4', partyCode } });
     if (student4Response.data.status === 200) console.log(`SUCCESS! Student4 added to server ${student4Response.data.data}`);
     else throw new Error('ERROR! Couldn\'t add Student1 to party');
 
-    // Attempt to add Student1 to party; EXPECT: Error 404, 'Party is already full'
-    const student5Response = await axios.get('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student5', partyCode } });
+    // Attempt to add Student5 to party; EXPECT: Error 404, 'Party is already full'
+    const student5Response = await axios.post('http://localhost:9090/server/joinPartyAsStudent', { params: { name: 'Student5', partyCode } });
     if (student5Response.data.status === 404) console.log('SUCCESS! Couldn\'t add Student5 since the party is already full');
     else throw new Error('ERROR! Somehow, the student got added to a full party');
   } catch (error) {
